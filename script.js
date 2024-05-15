1f91f0833a2692a1099a0fa29bcc2eb4d5d58c05
Pentaganos <3890060+Pentaganos@users.noreply.github.com>
2024-05-15 22:20:21 +0800

    Added 5 rounds and a score system.

--- script.js
+++ script.js
@@ -41,30 +41,35 @@ let humanScore=0;
 let computerScore=0;
 
 
-// plays one round of the whole game.
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === computerChoice) ||
-        (computerChoice === humanChoice)) {
-        console.log("Tie!");
-    } else if ((humanChoice === "Rock" && computerChoice === "Scissors") ||
-               (humanChoice === "Paper" && computerChoice === "Rock") ||
-               (humanChoice === "Scissors" && computerChoice === "Paper")) {
-        console.log("You Win! " + humanChoice + " beats " + computerChoice + ".");
-    } else {
-        console.log("You Lose! " + computerChoice + " beats " + humanChoice + ".");
-    }
-}
-
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
-
-
-playRound(humanSelection, computerSelection);
-
 
 //plays 5 rounds of the game.
 function playgame() {
+    // settings human and computer score to 0
+    let humanScore=0;
+    let computerScore=0;
+
     for (let index = 0; index < 5; index++) {
-        playRound(humanSelection, computerSelection);        
+
+        function playRound(humanChoice, computerChoice) {
+            if ((humanChoice === computerChoice) ||
+                (computerChoice === humanChoice)) {
+                console.log("Tie!");
+            } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+                       (humanChoice === "paper" && computerChoice === "rock") ||
+                       (humanChoice === "scissors" && computerChoice === "paper")) {
+                console.log("You Win! " + humanChoice + " beats " + computerChoice + ".");
+                humanScore++;
+            } else {
+                console.log("You Lose! " + computerChoice + " beats " + humanChoice + ".");
+                computerScore++;
+            }
+        }
+
+        let computerSelection = getComputerChoice();
+        let humanSelection = getHumanChoice();
+        playRound(humanSelection, computerSelection);
+        console.log("Your score is " + humanScore + " Computers score is " + computerScore);
     }
 }
+
+playgame();
