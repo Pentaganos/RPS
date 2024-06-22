1be40bb474965b6d17bfe6f0e1b118c95d57d378
Pentaganos <3890060+Pentaganos@users.noreply.github.com>
2024-06-22 18:18:02 +0800

    Basic ui added for RPS game from the odin project

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-//Get Computer Choice using math.random which returns a number then convert that number to rock paper or scissors
+//event listener for each button to play the game and stores the playerselection from the button clicked
+let playerSelection;
+let computerSelection;
+let humanScore=0;
+let computerScore=0;
+let roundNumber=0;
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        roundNumber++;
+        updateScore();
+        if (humanScore === 5 || computerScore === 5) {
+            if (humanScore > computerScore) {
+            } else if (humanScore < computerScore) {
+            } else {
+            }
+            humanScore=0;
+            computerScore=0;
+            roundNumber=0;
+        }
+    });
+});
+
+//Get Computer Choice using math.random which returns a number then convert that number to rock paper or scissors
 function getComputerChoice() {
     let randomComputerInt = Math.floor(Math.random() * 3);
 
@@ -18,58 +44,42 @@ function getComputerChoice() {
         default:
             getComputerChoiceString = "INVALID";
     }
-    console.log(getComputerChoiceString)
     return getComputerChoiceString;
 }
 
-// console.log(getComputerChoice());
-
-
-// Get human choice from an input (prompt) there is no sanitization on this.
-function getHumanChoice() {
-    let getHumanChoiceString = prompt("Please enter either Rock, Paper or Scissors");
-//convert to lower case
-    getHumanChoiceString = getHumanChoiceString.toLocaleLowerCase();
-    return getHumanChoiceString;
-}
-
-//  console.log(getHumanChoice());
-
-
-// settings human and computer score to 0
-let humanScore=0;
-let computerScore=0;
-
-
-
-//plays 5 rounds of the game.
-function playgame() {
-    // settings human and computer score to 0
-    let humanScore=0;
-    let computerScore=0;
-
-    for (let index = 0; index < 5; index++) {
-
-        function playRound(humanChoice, computerChoice) {
-            if ((humanChoice === computerChoice) ||
-                (computerChoice === humanChoice)) {
-                console.log("Tie!");
-            } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
-                       (humanChoice === "paper" && computerChoice === "rock") ||
-                       (humanChoice === "scissors" && computerChoice === "paper")) {
-                console.log("You Win! " + humanChoice + " beats " + computerChoice + ".");
-                humanScore++;
-            } else {
-                console.log("You Lose! " + computerChoice + " beats " + humanChoice + ".");
-                computerScore++;
-            }
-        }
-
-        let computerSelection = getComputerChoice();
-        let humanSelection = getHumanChoice();
-        playRound(humanSelection, computerSelection);
-        console.log("Your score is " + humanScore + " Computers score is " + computerScore);
+//plays a round of the game
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === computerChoice) ||
+        (computerChoice === humanChoice)) {
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
+    } else {
+        computerScore++;
     }
 }
 
-playgame();
+function updateScore() {
+    const container = document.querySelector('#container');
+    const content = document.createElement('div');
+    content.style.textAlign = 'center';
+    content.classList.add('content');
+    content.textContent = "Your score is " + humanScore + " Computers score is " + computerScore;
+    // code for winner after x rounds
+    if (humanScore === 5 || computerScore === 5) {
+        if (humanScore > computerScore) {
+            content.textContent = "You Win! " + humanScore + " to " + computerScore;
+        } else if (humanScore < computerScore) {
+            content.textContent = "You Lose! " + computerScore + " to " + humanScore;
+        } else {
+            content.textContent = "Tie! " + humanScore + " to " + computerScore;
+        }
+        humanScore=0;
+        computerScore=0;
+        roundNumber=0;
+    }
+    container.appendChild(content);
+}
+
+updateScore();
